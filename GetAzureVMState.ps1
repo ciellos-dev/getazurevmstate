ab3128d725675ffb45e29c5ea486d3337116de58
ciellos-dev <67259746+ciellos-dev@users.noreply.github.com>
2022-04-20 23:43:46 +0200

    Update GetAzureVMState.ps1

--- GetAzureVMState.ps1
+++ GetAzureVMState.ps1
@@ -11,64 +11,19 @@ $azurePassword = ConvertTo-SecureString $AzureClientSecret -AsPlainText -Force
 $psCred = New-Object System.Management.Automation.PSCredential($AzureClientId , $azurePassword)
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-# This is requried by Find-Module, by doing it beforehand we remove some warning messages
-Write-Host "Installing PowerShell modules d365fo.tools and Az" 
-#Check Modules installed
-$NuGet = Get-PackageProvider -Name nuget -ErrorAction SilentlyContinue
-$Az = Get-InstalledModule -Name Az -ErrorAction SilentlyContinue
-$DfoTools = Get-InstalledModule -Name d365fo.tools -ErrorAction SilentlyContinue
 
-if(Get-PSRepository -Name PSGallery1 -ErrorAction SilentlyContinue)
-{
-    Unregister-PSRepository -Name PSGallery1
-}
 
-if(Get-PSRepository -Name PSGalleryNew -ErrorAction SilentlyContinue)
-{
-    Unregister-PSRepository -Name PSGalleryNew
-}
+$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+Set-Alias -Name az -Value "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd"
+$AzureRMAccount = az login --service-principal -u $AzureClientId -p $AzureClientSecret --tenant $AzureTenantId
 
-Register-PSRepository -Name PSGalleryNew -SourceLocation https://www.powershellgallery.com/api/v2/ -PublishLocation https://www.powershellgallery.com/api/v2/package/ -ScriptSourceLocation https://www.powershellgallery.com/api/v2/items/psscript/ -ScriptPublishLocation https://www.powershellgallery.com/api/v2/package/ -InstallationPolicy Trusted -PackageManagementProvider NuGet
-Set-PSRepository -Name PSGalleryNew -InstallationPolicy Trusted
-
-if([string]::IsNullOrEmpty($NuGet))
-{
-    Write-Host "NuGet not installed. Installing..." 
-    Install-PackageProvider nuget -Verbose -Force -Confirm:$false
-}
-
-if([string]::IsNullOrEmpty($Az))
-{
-    Write-Host "Az not installed. Installing..." 
-    Install-Module -Name Az -AllowClobber -Scope CurrentUser -Verbose -Force -Confirm:$False -SkipPublisherCheck 
-}
-else
-{
-    Write-Host "Az already installed. Updating..." 
-    Update-Module -Name Az -Verbose
-}
-if([string]::IsNullOrEmpty($DfoTools))
-{
-    Write-Host "d365fo.tools not installed. Installing..." 
-    Install-Module -Name d365fo.tools -Repository PSGalleryNew -Scope CurrentUser -Verbose -Force -Confirm:$false
-}
-else
-{
-    Write-Host "d365fo.tools already installed. Updating..."
-    Update-Module -Name d365fo.tools -Verbose
-}
-
-Import-Module Az -Scope Global -Verbose 
-
-$AzureRMAccount = Add-AzAccount -Credential $psCred -ServicePrincipal -TenantId $AzureTenantId -Verbose 
 
 if ($AzureRMAccount) { 
     #Do Logic
     Write-Host "== Logged in == $AzureTenantId "
 
     Write-Host "Getting Azure VM State $AzureVMName"
-    $VMStats = (Get-AzVM -Name "$AzureVMName" -ResourceGroupName "$AzureVMResourceGroup" -Status -Verbose).Statuses
-    $PowerState = ($VMStats | Where Code -Like 'PowerState/*')[0].Code.Split("/")[1]
-    Write-Host "....state is" $PowerState
+    $PowerState = az vm list -d --query "[?name=='$($AzureVMName)'].powerState"
+    Write-Host "....state is" $PowerState.Trim().Trim("[").Trim("]").Trim('"')
     return $PowerState
 }
