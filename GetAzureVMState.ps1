033cfb8717d1325777477e86bbd684d18bf48bb7
ciellos-dev <67259746+ciellos-dev@users.noreply.github.com>
2022-04-20 18:11:07 +0200

    Update GetAzureVMState.ps1

--- GetAzureVMState.ps1
+++ GetAzureVMState.ps1
@@ -13,31 +13,37 @@ $psCred = New-Object System.Management.Automation.PSCredential($AzureClientId , 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # This is requried by Find-Module, by doing it beforehand we remove some warning messages
-Write-Host "Installing PowerShell modules d365fo.tools and AzureRM" 
+Write-Host "Installing PowerShell modules d365fo.tools and Az" 
 #Check Modules installed
 $NuGet = Get-PackageProvider -Name nuget -ErrorAction SilentlyContinue
 $Az = Get-InstalledModule -Name Az -ErrorAction SilentlyContinue
 $DfoTools = Get-InstalledModule -Name d365fo.tools -ErrorAction SilentlyContinue
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
 if([string]::IsNullOrEmpty($NuGet))
 {
+    Write-Host "NuGet not installed. Installing..." 
     Install-PackageProvider nuget -Scope CurrentUser -Verbose -Force -Confirm:$false
 }
 
 if([string]::IsNullOrEmpty($Az))
 {
+    Write-Host "Az not installed. Installing..." 
     Install-Module -Name Az -AllowClobber -Scope CurrentUser -Verbose -Force -Confirm:$False -SkipPublisherCheck 
 }
 else
 {
+    Write-Host "Az already installed. Updating..." 
     Update-Module -Name Az -AllowClobber -Verbose
 }
 if([string]::IsNullOrEmpty($DfoTools))
 {
+    Write-Host "d365fo.tools not installed. Installing..." 
     Install-Module -Name d365fo.tools -AllowClobber -Scope CurrentUser -Verbose -Force -Confirm:$false
 }
 else
 {
+    Write-Host "d365fo.tools already installed. Updating..."
     Update-Module -Name d365fo.tools -AllowClobber -Verbose
 }
 
