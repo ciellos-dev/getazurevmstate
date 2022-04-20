956c525bca996b0bc60f2358bc020974a6a43748
ciellos-dev <67259746+ciellos-dev@users.noreply.github.com>
2022-04-20 23:58:41 +0200

    Update GetAzureVMState.ps1

--- GetAzureVMState.ps1
+++ GetAzureVMState.ps1
@@ -29,7 +29,7 @@ if ($AzureRMAccount) {
     Write-Host "== Logged in == $AzureTenantId "
 
     Write-Host "Getting Azure VM State $AzureVMName"
-    $PowerState = az vm list -d --query "[?name=='$($AzureVMName)'].powerState"
-    Write-Host "....state is" $PowerState.Trim().Trim("[").Trim("]").Trim('"').Trim("VM ")
+    $PowerState = (az vm list -d --query "[?name=='$($AzureVMName)'].powerState").Trim().Trim("[").Trim("]").Trim('"').Trim("VM ")
+    Write-Host "....state is" $PowerState
     return $PowerState
 }
